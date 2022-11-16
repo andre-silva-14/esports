a283a14a6fd775eeedfe7be358159e7c3caf0480
andre-silva-14 <silva.andre1997@gmail.com>
2022-11-16 20:08:03 +0100

    Add Game Banner Transition

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -12,10 +12,10 @@ export function GameBanner(props: GameBannerProps) {
     <Link
       to={`/${props.id}`}
       state={{ title: props.title, bannerUrl: props.bannerUrl }}
-      className="relative rounded-lg overflow-hidden"
+      className="relative rounded-lg overflow-hidden [&:hover>div]:pb-6"
     >
       <img src={props.bannerUrl} alt={props.title} />
-      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 transition-all duration-300">
         <strong className="font-bold text-white block">{props.title}</strong>
         <span className="text-zinc-300 text-sm block mt-1">
           {props.adsCount} listing(s)
